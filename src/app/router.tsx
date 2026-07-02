61be115cbbb8a0226a406990d94de272f25c08f3
revoly afanasev <rewoly@mail.ru>
2026-07-02 22:11:22 +0900

    Добавил маршрут

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,10 @@
 import { Navigate, createBrowserRouter } from 'react-router-dom';
-import { AppShell } from '../../components/layout/AppShell';
-import { LoginPage } from '../../features/auth/LoginPage';
-import { CentralDashboardPage } from '../../features/central/dashboard/CentralDashboardPage';
-import { LocalDiagnosticsPage } from '../../features/local/diagnostics/LocalDiagnosticsPage';
-import { LocalOperatorDashboardPage } from '../../features/local/operator/LocalOperatorDashboardPage';
-import { NotFoundPage } from '../../features/NotFoundPage';
+import { AppShell } from '../components/layout/AppShell';
+import { LoginPage } from '../features/auth/LoginPage';
+import { CentralDashboardPage } from '../features/central/dashboard/CentralDashboardPage';
+import { LocalDiagnosticsPage } from '../features/local/diagnostics/LocalDiagnosticsPage';
+import { LocalOperatorDashboardPage } from '../features/local/operator/LocalOperatorDashboardPage';
+import { NotFoundPage } from '../features/NotFoundPage';
 
 export const router = createBrowserRouter([
   {
